f929db105a134a7cc54d7c6cd78fccd23b9a6b53
ex_fulin <Ermu2333>
2019-01-30 10:52:26 +0800

    init-gh-pages

---
+++ precache-manifest.c348b5ade56a307d2c23dad258dcf9e5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4baf056bdf252faa4230ffd036b912b3",
+    "url": "/static/media/YUI-again.4baf056b.mp3"
+  },
+  {
+    "revision": "b30a88b4911c786a9257",
+    "url": "/static/css/main.c727a484.chunk.css"
+  },
+  {
+    "revision": "9789bce3e2ba6d78043b923a1a6f8cf4",
+    "url": "/static/media/banner3.9789bce3.jpg"
+  },
+  {
+    "revision": "c4cb4328563fe8e4f945",
+    "url": "/static/js/1.c4cb4328.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29802a195c21f1c282fbcbcf4f03a85c",
+    "url": "/static/media/2.29802a19.jpg"
+  },
+  {
+    "revision": "b30a88b4911c786a9257",
+    "url": "/static/js/main.b30a88b4.chunk.js"
+  },
+  {
+    "revision": "d4c01ef70b0d04dff738e011390b55ac",
+    "url": "/static/media/YUI-GLORIA.d4c01ef7.mp3"
+  },
+  {
+    "revision": "6792ad5f899b15ce7f04e9dcb704951e",
+    "url": "/static/media/1.6792ad5f.jpg"
+  },
+  {
+    "revision": "0c633b0bc16e6dd19b4d997df5f8bb1d",
+    "url": "/static/media/banner1.0c633b0b.jpg"
+  },
+  {
+    "revision": "72e9767ec73664d23a397185689b2d7e",
+    "url": "/static/media/banner2.72e9767e.jpg"
+  },
+  {
+    "revision": "c4cb4328563fe8e4f945",
+    "url": "/static/css/1.ac7649ac.chunk.css"
+  },
+  {
+    "revision": "1b1239633ec289e0ed086707de3ee685",
+    "url": "/index.html"
+  }
+];
